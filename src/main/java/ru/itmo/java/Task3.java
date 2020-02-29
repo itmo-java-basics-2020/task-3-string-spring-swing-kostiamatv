da80b87d5336d338c31d537ae279452d11c123d1
(◕‿◕) <kostiamatv@gmail.com>
2020-02-29 12:14:06 +0300

    Style fixed

--- src/main/java/ru/itmo/java/Task3.java
+++ src/main/java/ru/itmo/java/Task3.java
@@ -10,11 +10,11 @@ public class Task3 {
      * Если инпут равен null - вернуть пустой массив
      */
     int[] getShiftedArray(int[] inputArray) {
-        if(inputArray == null || inputArray.length == 0){
+        if (inputArray == null || inputArray.length == 0) {
             return new int[0];
         }
         int last = inputArray[inputArray.length - 1];
-        for(int i = inputArray.length - 1; i > 0; i--){
+        for (int i = inputArray.length - 1; i > 0; i--) {
             inputArray[i] = inputArray[i - 1];
         }
         inputArray[0] = last;
@@ -24,16 +24,16 @@ public class Task3 {
     /**
      * Напишите функцию, которая принимает массив целых чисел и возвращает максимальное значение произведения двух его элементов.
      * Если массив состоит из одного элемента, то функция возвращает этот элемент.
-     *
+     * <p>
      * Если входной массив пуст или равен null - вернуть 0
-     *
+     * <p>
      * Пример: 2 4 6 -> 24
      */
     int getMaxProduct(int[] inputArray) {
-        if(inputArray == null || inputArray.length == 0){
+        if (inputArray == null || inputArray.length == 0) {
             return 0;
         }
-        if(inputArray.length == 1){
+        if (inputArray.length == 1) {
             return inputArray[0];
         }
         Arrays.sort(inputArray);
@@ -45,42 +45,42 @@ public class Task3 {
      * Напишите функцию, которая вычисляет процент символов 'A' и 'B' (латиница) во входной строке.
      * Функция не должна быть чувствительна к регистру символов.
      * Результат округляйте путем отбрасывания дробной части.
-     *
+     * <p>
      * Пример: acbr -> 50
      */
     int getABpercentage(String input) {
-        if(input == null || input == ""){
+        if (input == null || input == "") {
             return 0;
         }
         var chararray = input.toLowerCase().toCharArray();
         Arrays.sort(chararray);
-        var k =0;
-        for(int i=0; i < chararray.length; i++){
-            if(chararray[i] == 'b' | chararray[i] =='a'){
+        var k = 0;
+        for (int i = 0; i < chararray.length; i++) {
+            if (chararray[i] == 'b' | chararray[i] == 'a') {
                 k++;
             }
-            if(chararray[i] > 'b'){
+            if (chararray[i] > 'b') {
                 break;
             }
         }
-        if(chararray.length == 0){
+        if (chararray.length == 0) {
             return 0;
         }
-        return k*100/chararray.length;
+        return k * 100 / chararray.length;
     }
 
     /**
      * Напишите функцию, которая определяет, является ли входная строка палиндромом
      */
-    boolean isPalindrome(String input){
-        if(input == ""){
+    boolean isPalindrome(String input) {
+        if (input == "") {
             return true;
         }
-        if(input == null){
+        if (input == null) {
             return false;
         }
-        for(int i = 0; i <= input.length()/2; i++){
-            if(input.charAt(i) != input.charAt(input.length() - i - 1)){
+        for (int i = 0; i <= input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
                 return false;
             }
         }
@@ -91,19 +91,18 @@ public class Task3 {
      * Напишите функцию, которая принимает строку вида "bbcaaaak" и кодирует ее в формат вида "b2c1a4k1",
      * где группы одинаковых символов заменены на один символ и кол-во этих символов идущих подряд в строке
      */
-    String getEncodedString(String input){
-        if(input == null || input == ""){
+    String getEncodedString(String input) {
+        if (input == null || input == "") {
             return "";
         }
-        var result= "" + input.charAt(0);
+        var result = "" + input.charAt(0);
         var cur_num = 1;
         var cur_char = input.charAt(0);
         var i = 1;
-        while (i < input.length()){
-            if (cur_char == input.charAt(i)){
+        while (i < input.length()) {
+            if (cur_char == input.charAt(i)) {
                 cur_num++;
-            }
-            else {
+            } else {
                 result += Integer.toString(cur_num);
                 cur_char = input.charAt(i);
                 result += cur_char;
@@ -123,7 +122,7 @@ public class Task3 {
      * isPermutation("abc", "Abc") == false;
      */
     boolean isPermutation(String one, String two) {
-        if(one == null || two == null || one == "" || two == ""){
+        if (one == null || two == null || one == "" || two == "") {
             return false;
         }
         var chars1 = one.toCharArray();
@@ -139,13 +138,13 @@ public class Task3 {
      * Строкой является последовательность символов длинной N, где N > 0
      */
     boolean isUniqueString(String s) {
-        if(s == null || s == ""){
+        if (s == null || s == "") {
             return false;
         }
         var array = s.toCharArray();
         Arrays.sort(array);
-        for(int i = 1; i < array.length; i++){
-            if (array[i] == array[i-1]){
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] == array[i - 1]) {
                 return false;
             }
         }
@@ -154,15 +153,15 @@ public class Task3 {
 
     /**
      * Напишите функцию, которая транспонирует матрицу. Только квадратные могут быть на входе.
-     *
+     * <p>
      * Если входной массив == null - вернуть пустой массив
      */
     int[][] matrixTranspose(int[][] m) {
-        if(m == null){
+        if (m == null) {
             return new int[0][0];
         }
-        for(int i = 0; i < m.length; i++){
-            for(int j = i + 1; j < m.length; j++){
+        for (int i = 0; i < m.length; i++) {
+            for (int j = i + 1; j < m.length; j++) {
                 m[i][j] += m[j][i];
                 m[j][i] = m[i][j] - m[j][i];
                 m[i][j] -= m[j][i];
@@ -174,21 +173,21 @@ public class Task3 {
     /**
      * Напиишите функцию, принимающую массив строк и символ-разделитель,
      * а возвращает одну строку состоящую из строк, разделенных сепаратором.
-     *
+     * <p>
      * Запрещается пользоваться функций join
-     *
+     * <p>
      * Если сепаратор == null - считайте, что он равен ' '
      * Если исходный массив == null -  вернуть пустую строку
      */
     String concatWithSeparator(String[] inputStrings, Character separator) {
-        if(inputStrings == null || inputStrings.length == 0){
+        if (inputStrings == null || inputStrings.length == 0) {
             return "";
         }
-        if(separator == null){
+        if (separator == null) {
             separator = ' ';
         }
         var result = inputStrings[0];
-        for(int i = 1; i < inputStrings.length; i ++){
+        for (int i = 1; i < inputStrings.length; i++) {
             result += separator + inputStrings[i];
         }
         return result;
@@ -198,16 +197,16 @@ public class Task3 {
      * Напишите функцию, принимающую массив строк и строку-перфикс и возвращающую кол-во строк массива с данным префиксом
      */
     int getStringsStartWithPrefix(String[] inputStrings, String prefix) {
-        if(inputStrings == null || inputStrings.length == 0 || prefix == null){
+        if (inputStrings == null || inputStrings.length == 0 || prefix == null) {
             return 0;
         }
         var num = 0;
-        for(int i = 0; i < inputStrings.length; i++){
-            if(prefix.length() <= inputStrings[i].length()){
-                num ++;
-                for(int j = 0; j < prefix.length(); j++){
-                    if(prefix.charAt(j) != inputStrings[i].charAt(j)){
-                        num --;
+        for (int i = 0; i < inputStrings.length; i++) {
+            if (prefix.length() <= inputStrings[i].length()) {
+                num++;
+                for (int j = 0; j < prefix.length(); j++) {
+                    if (prefix.charAt(j) != inputStrings[i].charAt(j)) {
+                        num--;
                         break;
                     }
                 }
